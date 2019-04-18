07403585b2d6c85c98cf08a8c9b2e1a131354064
Tobias Schoofs <tobias.schoofs@gmx.net>
2019-04-18 16:16:50 +0100

    [develop] lock uses lock...

--- ipc.lua
+++ ipc.lua
@@ -33,29 +33,53 @@
 local ipc = {}
 
 function ipc.createlock(lk)
+  nowdb.execute_(string.format(
+    [[create lock %s if not exists]], lk))
 end
 
 function ipc.droplock(lk)
+  nowdb.execute_(string.format(
+    [[drop lock %s if exists]], lk))
 end
 
-function ipc.lock(lk,mode)
-  local m = not mode and 'w' or mode
-  if m ~= 'r' and m ~= 'w' then error('unknown locking mode') end
-  -- lock
+function ipc.lock(lk,mode,tmo)
+  local modeclause = ''
+  if not mode or mode == 'w' then
+     modeclause = 'for writing'
+  elseif mode == 'r' then
+     modeclause = 'for reading'
+  else
+     error(string.format("unknown locking mode: '%s'", tostring(mode)))
+  end
+  local tmoclause = ''
+  if tmo then
+     tmoclause = string.format("set timeout = %d", tmo)
+  end
+  local rc, r = nowdb.pexecute(string.format(
+    [[lock %s %s %s]], lk, modeclause, tmoclause))
+  if rc ~= nowdb.OK then
+     if rc == nowdb.TIMEOUT then
+        return nowdb.TIMEOUT
+     else
+        nowdb.raise(rc, r)
+     end
+  end
+  return nowdb.OK
 end
 
 function ipc.unlock(lk)
-  -- unlock
+  nowdb.execute_(string.format(
+    [[unlock %s]], lk))
 end
 
 function ipc.withlock(lk, mode, f, ...)
-  ipc.lock(lk,mode)
-  ok, res = pcall(f, ...)
-  if not ok then
-     ipc.unlock(lk)
-     error(res)
+  local rc = ipc.lock(lk,mode)
+  if rc ~= nowdb.OK then 
+     nowdb.raise(rc, string.format("on locking %s", lk))
   end
+  local ok, res = pcall(f, ...)
   ipc.unlock(lk)
+  if not ok then error(res) end
   return res
 end
 
